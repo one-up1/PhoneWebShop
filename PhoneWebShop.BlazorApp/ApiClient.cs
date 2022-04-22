ed473199a73bf3dec3a827826710ce98d86a0875
Ferenc Dykstra <oneup1@gmail.com>
2022-04-22 13:00:04 +0200

    - ApiClient

---
+++ PhoneWebShop.BlazorApp/ApiClient.cs
@@ -0,0 +1,30 @@
+﻿using PhoneWebShop.Domain.Entities;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace PhoneWebShop.BlazorApp
+{
+    public class ApiClient
+    {
+        private const string URL = "https://localhost:44337/api";
+
+        private HttpClient client;
+
+        public ApiClient()
+        {
+            client = new HttpClient();
+        }
+
+        public Task<IEnumerable<Phone>> GetPhones()
+        {
+            return client.GetFromJsonAsync<IEnumerable<Phone>>(URL + "/Phones/Search");
+        }
+
+        public Task<Phone> GetPhone(int id)
+        {
+            return client.GetFromJsonAsync<Phone>(URL + "/Phones/" + id);
+        }
+    }
+}
